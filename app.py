dd090442abe85b3f4d48087ebceec636d9f3ad35
tsenghc <danny2953053@gmail.com>
2022-03-14 00:54:44 +0800

    感測資料回傳至API

--- app.py
+++ app.py
@@ -1,47 +1,37 @@
+import random
+
 from flask import Flask, jsonify, render_template
 
-from fdk300 import FDK300
-from fdk400 import FDK400
-from m170 import M170
-from mtk_a1 import MTKA1
 
 app = Flask(__name__)
 
 
 @ app.route('/fdk300')
 def fdk300():
-    fdk300 = FDK300()
-    _temp = fdk300.get_sensor_data()
-    sensor_data = {'temperature': _temp['temperature']}
+    sensor_data = {'temperature': random.randint(10,100)}
     return jsonify(sensor_data), 200
 
 
 @ app.route('/fdk400')
 def fdk400():
-    fdk400 = FDK400()
-    _temp = fdk400.get_sensor_data()
     sensor_data = {'pressure_S': 0, 'pressure_D': 0}
-    sensor_data['pressure_S'] = _temp['pressure_S']
-    sensor_data['pressure_D'] = _temp['pressure_D']
+    sensor_data['pressure_S'] = random.randint(10,100)
+    sensor_data['pressure_D'] = random.randint(10,100)
     return jsonify(sensor_data), 200
 
 
 @ app.route('/mtka1')
 def mtka1():
-    scale = MTKA1()
     sensor_data = {'weight': 0}
-    _temp = scale.get_sensor_data()
-    sensor_data['weight'] = _temp['weight']
+    sensor_data['weight'] = random.randint(10,100)
     return jsonify(sensor_data), 200
 
 
 @ app.route('/m170')
 def m170():
-    m170 = M170()
     sensor_data = {'oxygen': 0, 'pulse': 0}
-    _temp = m170.get_sensor_data()
-    sensor_data['oxygen'] = _temp['oxygen']
-    sensor_data['pulse'] = _temp['pulse']
+    sensor_data['oxygen'] = random.randint(10,100)
+    sensor_data['pulse'] = random.randint(10,100)
     return jsonify(sensor_data), 200
 
 
@@ -53,4 +43,4 @@ def index():
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.jinja_env.auto_reload = True
-    app.run(debug=True, threaded=True)
+    app.run(host='127.0.0.1', debug=True, port=5000)
